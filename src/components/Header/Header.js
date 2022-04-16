3b586fd72a5b067829a40144875d39c297e0e027
Tarequl Islam <tareque.dev@gmail.com>
2022-04-17 01:53:39 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div style={{ height: "500px" }} className="header">
+      <div className="content top-5">
+        <h2 className="text-white">Best Food Waiting For Your balley</h2>
+        <p className="text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem id nihil debitis excepturi quas placeat corporis, dolorum officiis dolore in, amet consequuntur sunt eveniet quo labore distinctio non. Ratione facilis numquam dolorem reiciendis id iusto modi amet nemo.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import NavPage from '../NavPage/NavPage';
+
+const Main = () => {
+     return (
+          <div>
+               <NavPage />
+               <Header />
+          </div>
+     );
+};
+
+export default Main;
